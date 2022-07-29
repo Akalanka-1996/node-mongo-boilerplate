8ef96b0dcb7ee0415d0824c8e0d036b630f40a7b
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-07-29 22:46:14 +0530

    post route changed

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const {getTasks, createTask, getTaskById, updateTask, deleteTask} = require('../controllers/taskController')
 
 router.route('/').get(getTasks)
-router.route('/create').post(createTask)
+router.route('/').post(createTask)
 router.route('/:id').get(getTaskById).put(updateTask).delete(deleteTask)
 
 module.exports = router
